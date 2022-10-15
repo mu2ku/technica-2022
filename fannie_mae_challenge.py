8f85b7a0d76fb9bebaac7de290d07e58102c7496
mu2ku <gmutuku@gmail.com>
2022-10-15 16:46:46 -0400

    commit initial sqlite

--- fannie_mae_challenge.py
+++ fannie_mae_challenge.py
@@ -1,4 +1,19 @@
 import csv
+import sqlite3
+
+def insert_into_table(input_file):
+    con = sqlite3.connect("test.db") #create connection to db
+    cur = con.cursor() #establish cursor
+    print("Connected to SQLite")
+    insert_query = """INSERT INTO test(gross_monthly_income, credit_card_payment, car_payment, student_loan, appraised_value, down_payment, loan_amount,monthly_mortage, credit) VALUES (?,?,?,?,?,?,?,?)")""" #code to insert into db
+    
+    with open(input_file, 'r') as file:
+        csvreader = csv.reader(file)
+        for row in csvreader:
+            cur.executemany(insert_query,row)
+        con.commit()
+        results = cur.fetchall()
+        print(results)
 
 def check_credit_rating(credit_score):
     '''return true if buyer is approved (credit score >= 640) and false otherwise'''
@@ -20,7 +35,9 @@ def check_front_end_debt_to_income(monthly_mortgage, gross_monthly_income):
 def is_approved(row):
     return 
 
-with open("technica_2022-HomeBuyerInfo.csv", 'r') as file:
-    csvreader = csv.reader(file)
-    for row in csvreader:
-        print(row)
+insert_into_table("technica_2022-HomeBuyerInfo.csv")
+
+# with open("technica_2022-HomeBuyerInfo.csv", 'r') as file:
+#     csvreader = csv.reader(file)
+#     for row in csvreader:
+#         print(row)
