25d291d3fbe559ee3d4e0606e7fee07377b1a21d
Grace M <91351273+mu2ku@users.noreply.github.com>
2022-10-16 10:34:51 -0400

    commit file to db functionality

--- email_data.py
+++ email_data.py
@@ -1,19 +1,19 @@
-import os
-import win32com.client as win32
+# import os
+# import win32com.client as win32
 
-olApp = win32.Dispatch('Outlook.Application')
-olNS = olApp.GetNameSpace('MAPI')
+# olApp = win32.Dispatch('Outlook.Application')
+# olNS = olApp.GetNameSpace('MAPI')
 
-def send_email(email_address):
-    print(email_address)
-    mail_item = olApp.CreateItem(0)
-    mail_item.Subject = 'Home Buyer Report'
-    mail_item.BodyFormat = 1
-    mail_item.Body = 'This report includes a .csv file with the home buyer data and two images showing a breakdown of approvals and disapprovals.'
-    mail_item.To = email_address
-    mail_item.Attachments.Add(os.path.join(os.getcwd(), 'homebuyerinfo.csv'))
-    mail_item.Attachments.Add(os.path.join(os.getcwd(), 'approved_percent.png'))
-    mail_item.Attachments.Add(os.path.join(os.getcwd(), 'whynotapproved.png'))
+# def send_email(email_address):
+#     print(email_address)
+#     mail_item = olApp.CreateItem(0)
+#     mail_item.Subject = 'Home Buyer Report'
+#     mail_item.BodyFormat = 1
+#     mail_item.Body = 'This report includes a .csv file with the home buyer data and two images showing a breakdown of approvals and disapprovals.'
+#     mail_item.To = email_address
+#     mail_item.Attachments.Add(os.path.join(os.getcwd(), 'homebuyerinfo.csv'))
+#     mail_item.Attachments.Add(os.path.join(os.getcwd(), 'approved_percent.png'))
+#     mail_item.Attachments.Add(os.path.join(os.getcwd(), 'whynotapproved.png'))
 
-    mail_item.Send()
+#     mail_item.Send()
 
